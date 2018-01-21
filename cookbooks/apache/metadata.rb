efed6b4768ab4ece21411aa1c54263267dcb66a2
Ed <veribaka@gmail.com>
2018-01-21 12:04:13 +0000

    Added motd and changed apache default for node attributes

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -4,9 +4,8 @@ maintainer_email 'veribaka@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
-version '0.1.0'
+version '0.2.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,9 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-#
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node ['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package 'apache2' do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
